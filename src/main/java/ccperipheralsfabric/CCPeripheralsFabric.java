97b68255117f64077b1c85954fc0c3ab08709073
Ben Dodd <ben-dodd@outlook.com>
2021-04-08 19:19:25 +0100

    Initial Environment Sensor

--- src/main/java/ccperipheralsfabric/CCPeripheralsFabric.java
+++ src/main/java/ccperipheralsfabric/CCPeripheralsFabric.java
@@ -1,6 +1,14 @@
 package ccperipheralsfabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +19,14 @@ public class CCPeripheralsFabric implements ModInitializer {
 	public static final String MOD_ID = "CCPeripheralsFabric";
 	public static final String MOD_NAME = "CCPeripheralsFabric";
 
+	public static final Block ENVIRONMENT_SENSOR = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+
 	@Override
 	public void onInitialize() {
 		log(Level.INFO, "Initializing");
+		Registry.register(Registry.BLOCK, new Identifier("ccperipherals", "environment_sensor"), ENVIRONMENT_SENSOR);
+		Registry.register(Registry.ITEM, new Identifier("ccperipherals", "environment_sensor"), new BlockItem(ENVIRONMENT_SENSOR, new FabricItemSettings().group(ItemGroup.MISC)));
+		log(Level.INFO, "Finished initializing");
 	}
 
 	public static void log(Level level, String message){
