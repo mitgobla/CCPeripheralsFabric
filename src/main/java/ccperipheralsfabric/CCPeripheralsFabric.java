983ee14e5338e73f149e6834becbd956484f8d93
Ben Dodd <ben-dodd@outlook.com>
2021-04-15 04:05:09 +0100

    Item Group

--- src/main/java/ccperipheralsfabric/CCPeripheralsFabric.java
+++ src/main/java/ccperipheralsfabric/CCPeripheralsFabric.java
@@ -4,6 +4,7 @@ import ccperipheralsfabric.common.peripheral.sensor.environment.BlockEnvironment
 import ccperipheralsfabric.common.peripheral.sensor.environment.TileEnvironmentSensor;
 import dan200.computercraft.shared.util.FixedPointTileEntityType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -11,7 +12,9 @@ import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
@@ -27,15 +30,22 @@ public class CCPeripheralsFabric implements ModInitializer {
 	public static final String MOD_ID = "CCPeripheralsFabric";
 	public static final String MOD_NAME = "CCPeripheralsFabric";
 
+	// Blocks
 	public static final BlockEnvironmentSensor ENVIRONMENT_SENSOR = new BlockEnvironmentSensor(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 	public static BlockEntityType<TileEnvironmentSensor> TILE_ENVIRONMENT_SENSOR;
+
+	// ItemGroup
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("ccperipherals", "general"),
+			() -> new ItemStack(ENVIRONMENT_SENSOR));
 
 	@Override
 	public void onInitialize() {
 		log(Level.INFO, "Initializing");
+		// Environment Sensor
 		Registry.register(Registry.BLOCK, new Identifier("ccperipherals", "environment_sensor"), ENVIRONMENT_SENSOR);
 		TILE_ENVIRONMENT_SENSOR = Registry.register(Registry.BLOCK_ENTITY_TYPE, "ccperipherals:environment_sensor", BlockEntityType.Builder.create(TileEnvironmentSensor::new, ENVIRONMENT_SENSOR).build(null));
-		Registry.register(Registry.ITEM, new Identifier("ccperipherals", "environment_sensor"), new BlockItem(ENVIRONMENT_SENSOR, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, new Identifier("ccperipherals", "environment_sensor"), new BlockItem(ENVIRONMENT_SENSOR, new FabricItemSettings().group(ITEM_GROUP)));
 		log(Level.INFO, "Finished initializing");
 	}
 
