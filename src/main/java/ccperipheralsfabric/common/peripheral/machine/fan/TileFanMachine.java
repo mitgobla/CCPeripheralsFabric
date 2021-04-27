69b40c940615873e4284be05deb80a3dbaed37ab
Ben Dodd <ben-dodd@outlook.com>
2021-04-27 22:53:45 +0100

    Add underwater fan particles

--- src/main/java/ccperipheralsfabric/common/peripheral/machine/fan/TileFanMachine.java
+++ src/main/java/ccperipheralsfabric/common/peripheral/machine/fan/TileFanMachine.java
@@ -4,15 +4,20 @@ import ccperipheralsfabric.CCPeripheralsFabric;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.IPeripheralTile;
 import dan200.computercraft.shared.common.TileGeneric;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.block.FluidDrainable;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Tickable;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -47,15 +52,36 @@ public class TileFanMachine extends TileGeneric implements IPeripheralTile, Tick
             Direction direction = this.getCachedState().get(BlockFanMachine.FACING);
             int distance = 0;
             for (int i = 1; i <= 5; i++) {
-                if (!this.world.getBlockState(this.getPos().offset(direction, i)).isAir()) break;
+                if (!this.world.getBlockState(this.getPos().offset(direction, i)).isAir() && !(this.world.getBlockState(this.getPos().offset(direction, i)).getBlock() instanceof FluidBlock)) break;
                 distance = i;
             }
+
+            for (int i = 1; i <= distance; i++) {
+                if (this.world.getBlockState(this.getPos().offset(direction, i)).getBlock() instanceof FluidBlock || this.world.getBlockState(this.getPos().offset(direction, i)).getBlock() instanceof FluidDrainable) {
+                    BlockPos particlePos = new BlockPos(this.getPos()).offset(direction, i).add(0.5, 0.5, 0.5);
+                    if (world.random.nextInt((i/distance)*1000 + 10) == 0) {
+                        Vec3i particleDir = direction.getVector();
+                        world.addParticle(ParticleTypes.BUBBLE, particlePos.getX() + this.world.random.nextDouble(), particlePos.getY() + this.world.random.nextDouble(), particlePos.getZ() + this.world.random.nextDouble(), particleDir.getX() * 2.0, particleDir.getY() * 2.0, particleDir.getZ() * 2.0);
+                    }
+
+                    if (world.random.nextInt(2000) == 0) {
+                        world.playSound(particlePos.getX(), particlePos.getY(), particlePos.getZ(), SoundEvents.BLOCK_BUBBLE_COLUMN_UPWARDS_AMBIENT, SoundCategory.BLOCKS, 0.2F + world.random.nextFloat() * 0.2F, 0.9F + world.random.nextFloat() * 0.15F, false);
+                    }
+                }
+            }
+
 
             Box box = new Box(this.getPos(), this.getPos().offset(direction, distance).add(1, 1, 1));
             List<Entity> entities = this.world.getEntitiesByClass(Entity.class, box, null);
             Vec3d dirVector = new Vec3d(direction.getOffsetX(), direction.getOffsetY(), direction.getOffsetZ());
             dirVector = dirVector.multiply(0.15);
             for (Entity entity : entities) {
+                if (entity instanceof PlayerEntity) {
+                    PlayerEntity player = (PlayerEntity) entity;
+                    if (player.isCreative() || player.isSpectator()) {
+                        continue;
+                    }
+                }
                 entity.setVelocity(entity.getVelocity().add(dirVector));
             }
         }
