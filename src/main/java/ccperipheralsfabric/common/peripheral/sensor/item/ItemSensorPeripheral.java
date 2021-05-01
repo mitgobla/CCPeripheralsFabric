a6bff2a9ba647b5549ffa19d61404f9c7a642649
Ben Dodd <ben-dodd@outlook.com>
2021-05-01 18:31:33 +0100

    getItems returns ItemType

--- src/main/java/ccperipheralsfabric/common/peripheral/sensor/item/ItemSensorPeripheral.java
+++ src/main/java/ccperipheralsfabric/common/peripheral/sensor/item/ItemSensorPeripheral.java
@@ -1,18 +1,18 @@
 package ccperipheralsfabric.common.peripheral.sensor.item;
 
+import ccperipheralsfabric.types.ItemType;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -73,6 +73,28 @@ public abstract class ItemSensorPeripheral implements IPeripheral {
             count += item.getStack().getCount();
         }
         return count;
+    }
+
+    @LuaFunction
+    public final ArrayList<ItemType> getItems() throws LuaException {
+        if (this.getWorld() != null && this.isEnabled() && !this.getWorld().isClient) {
+            return getItemsMethod();
+        }
+        return null;
+    }
+
+    public synchronized ArrayList<ItemType> getItemsMethod() throws LuaException {
+        World world = this.getWorld();
+        Box box = new Box(new BlockPos(this.getPosition()).add(-4, 0, -4), new BlockPos(this.getPosition()).add(4, 1, 4));
+        List<ItemEntity> items = world.getEntitiesByClass(ItemEntity.class, box, null);
+        if (items.size() > 0) {
+            ArrayList<ItemType> itemTypes = new ArrayList<>();
+            for (ItemEntity item : items) {
+                itemTypes.add(new ItemType(item));
+            }
+            return itemTypes;
+        }
+        return null;
     }
 
     public void broadcastItems(int count) {
