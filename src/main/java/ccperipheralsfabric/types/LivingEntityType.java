f0fac38e2d182078f8cbac2fdf83e79992aef3cf
Ben Dodd <ben-dodd@outlook.com>
2021-05-02 19:38:21 +0100

    Update EntityType to LivingEntityType

--- src/main/java/ccperipheralsfabric/types/LivingEntityType.java
+++ src/main/java/ccperipheralsfabric/types/LivingEntityType.java
@@ -1,28 +1,34 @@
 package ccperipheralsfabric.types;
 
 import dan200.computercraft.api.lua.LuaFunction;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.registry.Registry;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class EntityType {
+public class LivingEntityType {
     private final String name;
     private final String identifier;
     private final String customName;
     private final float health;
+    private final float maxHealth;
+    private final boolean isHostile;
 
-    public EntityType(LivingEntity livingEntity) {
-        this.name = new TranslatableText(livingEntity.getType().getTranslationKey()).toString();
+    public LivingEntityType(LivingEntity livingEntity) {
+        this.name = I18n.translate(livingEntity.getType().getTranslationKey());
         this.identifier = Registry.ENTITY_TYPE.getId(livingEntity.getType()).toString();
         if (livingEntity.getCustomName() != null) {
-            this.customName = livingEntity.getCustomName().toString();
+            this.customName = livingEntity.getCustomName().asString();
         } else {
             this.customName = this.name;
         }
         this.health = livingEntity.getHealth();
+        this.maxHealth = livingEntity.getMaxHealth();
+        this.isHostile = livingEntity instanceof HostileEntity;
     }
 
     @LuaFunction
@@ -46,12 +52,24 @@ public class EntityType {
     }
 
     @LuaFunction
+    public float getMaxHealth() {
+        return maxHealth;
+    }
+
+    @LuaFunction
+    public boolean isHostile() {
+        return isHostile;
+    }
+
+    @LuaFunction
     public Map<String, Object> toTable() {
         Map<String, Object> data = new HashMap<>();
         data.put("name", this.name);
         data.put("identifier", this.identifier);
         data.put("custom_name", this.customName);
         data.put("health", this.health);
+        data.put("max_health", this.maxHealth);
+        data.put("hostile", this.isHostile);
         return data;
     }
 }
