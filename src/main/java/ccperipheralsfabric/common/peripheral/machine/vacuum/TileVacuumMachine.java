757006978fe291ff1fa1c1c18553f1dbd1ba186b
Ben Dodd <ben-dodd@outlook.com>
2021-05-02 11:36:26 +0100

    Vacuum uses line of sight

--- src/main/java/ccperipheralsfabric/common/peripheral/machine/vacuum/TileVacuumMachine.java
+++ src/main/java/ccperipheralsfabric/common/peripheral/machine/vacuum/TileVacuumMachine.java
@@ -5,12 +5,16 @@ import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.IPeripheralTile;
 import dan200.computercraft.shared.common.TileGeneric;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Tickable;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -42,20 +46,24 @@ public class TileVacuumMachine extends TileGeneric implements IPeripheralTile, T
 
     private void pullEntities() {
         if (this.world != null ) {
-            Box box = new Box(this.getPos().add(-4.0, -4.0, -4.0), this.getPos().add(4.0, 4.0, 4.0));
-            List<Entity> entities = this.world.getEntitiesByClass(Entity.class, box, null);
+            Box box = new Box(this.getPos().add(-4.0, -4.0, -4.0), this.getPos().add(5.0, 5.0, 5.0));
+            List<Entity> entities = this.world.getEntitiesByClass(LivingEntity.class, box, null);
             Vec3d dirVector = new Vec3d(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ()).add(0.5, 0.5, 0.5);
             Vec3d normVector = new Vec3d(0, 0, 0);
             for (Entity entity : entities) {
+                normVector = entity.getPos().subtract(dirVector).normalize();
+                if (world.raycast(new RaycastContext(dirVector.add(normVector).add(normVector), entity.getPos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, entity)).getType() != HitResult.Type.MISS) {
+                    continue;
+                }
+
                 if (entity instanceof PlayerEntity) {
                     PlayerEntity player = (PlayerEntity) entity;
                     if (player.isCreative() || player.isSpectator()) {
                         continue;
                     }
                 }
-                normVector = dirVector.subtract(entity.getPos()).normalize();
                 normVector = normVector.multiply(0.12);
-                entity.setVelocity(entity.getVelocity().add(normVector));
+                entity.setVelocity(entity.getVelocity().subtract(normVector));
             }
         }
     }
